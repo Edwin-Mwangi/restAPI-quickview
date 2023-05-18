e53c68ed99fe2a6a4817a3ca04dc7f0c73807c70
Edwin-Mwangi <edwinmwangi138@gmail.com>
2023-05-18 13:12:00 +0300

    minor debugs

--- routes/api.js
+++ routes/api.js
@@ -8,16 +8,22 @@ router.get('/ninjas', function(req, res){
 
 //add a new ninja
 router.post('/ninjas', function(req, res){
+    //req data is posted from POSTMAN in viewport
     //from body parser to get req data
     console.log(req.body)
-    res.send({type: 'POST'})
+    res.send({
+        type: 'POST',
+        //to view the req data...in postman
+        name: req.body.name,
+        rank: req.body.rank
+    })
 })
 //update nninja list
-router.put('/ninjas', function(req, res){
+router.put('/ninjas/:id', function(req, res){
     res.send({type: 'PUT'})
 })
 //delete a ninja
-router.delete('/ninjas', function(req, res){
+router.delete('/ninjas/:id', function(req, res){
     res.send({type: 'DELETE'})
 })
 
